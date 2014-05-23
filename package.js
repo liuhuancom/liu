01ced72cd5808cad7ac38ff8233f013c32a31e6e
liuhuancom <liuhuancom@126.com>
2014-05-23 10:51:01 +0800

    Update package.js

--- package.js
+++ package.js
@@ -2,10 +2,14 @@ Package.describe({
   summary: "simple spinner_liu package for Meteor"
 });
 
-Package.on_use(function (api, where){
+
+
+
+Package.on_use(function (api, where) {
   api.use(['templating'], 'client');
   
-  api.add_files(['spiner.js', 'spiner.html'], 'client');
-
-
+  
+  api.add_files([
+    'spinner_liu.html', 
+    'spinner_liu.js'], 'client');
 });
